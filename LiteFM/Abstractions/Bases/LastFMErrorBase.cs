72053d748195cd045daf5826d45b59c18910cb85
Raspberry-Monster <lele2194512339@outlook.com>
2026-01-15 12:39:35 +0800

    chores: Move ErrorBase's Content to LastFMJsonError

--- LiteFM/Abstractions/Bases/LastFMErrorBase.cs
+++ LiteFM/Abstractions/Bases/LastFMErrorBase.cs
@@ -4,9 +4,6 @@ namespace LiteFM.Abstractions.Bases
 {
     public class LastFMErrorBase : LastFMResponseBase
     {
-        [JsonPropertyName("message")]
-        public string Message { get; set; } = string.Empty;
-        [JsonPropertyName("error")]
-        public int Error { get; set; } = 0;
+
     }
 }
--- LiteFM/Abstractions/LastFMError.cs
+++ LiteFM/Abstractions/LastFMError.cs
@@ -1,9 +1,13 @@
 ﻿using LiteFM.Abstractions.Bases;
+using System.Text.Json.Serialization;
 
 namespace LiteFM.Abstractions
 {
     public sealed class LastFMJsonError : LastFMErrorBase
     {
-
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+        [JsonPropertyName("error")]
+        public int Error { get; set; } = 0;
     }
 }
